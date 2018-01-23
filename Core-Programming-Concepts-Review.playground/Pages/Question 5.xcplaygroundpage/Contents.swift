7bf4a076933abbbd519f3896c93b6d6060043894
Zhusupbekova, May <mzhusupbekova@lcs-mba3239.lcs.on.ca>
2018-01-23 11:57:45 -0500

    5th exercise - if-else boolean

--- Core-Programming-Concepts-Review.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
+++ Core-Programming-Concepts-Review.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
@@ -23,7 +23,11 @@ let isRaining = true
 let isSunny = true
 
 // Answer question 5 below
-
+if isSunny && temperatureInCelsius > 20 {
+    print ("I'm going for walk")
+} else {
+    print ("I'll stay inside today.")
+}
 
 /*:
  ## Now share your understanding
